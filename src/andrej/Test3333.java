71068d68257382c815137b8e0290aed7d944184e
AndrejFroehlich <afroehlich@student.tgm.ac.at>
2017-06-05 20:30:13 +0200

    3-MAL

--- src/andrej/Test3333.java
+++ src/andrej/Test3333.java
@@ -4,6 +4,8 @@ public class Test3333 {
  
 	public static void main(String[] args) {
 		System.out.println("Guten Morgen Herr Fröhlich");
+		System.out.println("Guten Morgen Herr Fröhlich");
+		System.out.println("Guten Morgen Herr Fröhlich");
 	}
 	
 }
